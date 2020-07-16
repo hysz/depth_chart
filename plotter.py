e7508611ad5483bf5def28eebc53c56189675e3b
Greg Hysen <greg.hysen@gmail.com>
2020-07-15 19:18:06 -0700

    merges before drawing the individuals

--- plotter.py
+++ plotter.py
@@ -14,8 +14,8 @@ def plot(name, prices, cumulative_depths):
     ax.fill_between(prices, 0, cumulative_depths)
     plt.ylabel('Depth')
     plt.xlabel('Price')
-    plt.show()
-    #plt.savefig('%s.png'%(name), bbox_inches='tight')
+    #plt.show()
+    plt.savefig('%s.png'%(name), bbox_inches='tight')
 
 # Fake response from 0x API
 '''
@@ -29,25 +29,25 @@ response_json = {
 
 raw_json = json.load(open('./raw'))
 
-response_json = {}
+response_json = raw_json
+'''
 for quote_list in raw_json['dexQuotes']:
     for quote in quote_list:
         if not quote["source"] in response_json:
             response_json[quote["source"]] = []
         response_json[quote["source"]].append({"input": float(quote["input"]), "output": float(quote["output"])})
 
-
-
+'''
 
 
 # Parse out sources
 sources = []
-for name,inouts in response_json.items():
-    cum_output = 0
+for name,inouts in response_json['depth'].items():
+    cum_output = 0.0
     depths = [Depth(0,0,0)]
     for inout in inouts:
-        depths.append(Depth(inout["input"], inout["output"], inout["output"] + cum_output))
-        cum_output += inout["output"]
+        depths.append(Depth(float(inout["input"]), float(inout["output"]), float(inout["output"]) + cum_output))
+        cum_output += float(inout["output"])
     sources.append(Source(name, depths))
   
 # print(sources)
@@ -59,11 +59,6 @@ def depths_to_xy(depths):
         prices.append(depth.input)
         cumulative_depths.append(depth.cum_output)
     return (prices, cumulative_depths)
-
-def print_individual(sources):
-    for source in sources:
-        (prices, cumulative_depths) = depths_to_xy(source.depths)
-        plot(source.name, prices, cumulative_depths)
 
 def compare_depths(d1, d2):
     if d1.input < d2.input:
@@ -84,11 +79,17 @@ def merge_and_sort(depths):
 
     return merged_depths
 
+def print_individual(sources):
+    for source in sources:
+        (prices, cumulative_depths) = depths_to_xy(merge_and_sort(source.depths))
+        plot(source.name, prices, cumulative_depths)
 
 def print_unified(sources):
     cumulative_depths = [depth for source in sources for depth in source.depths]  
     (prices, cumulative_depths) = depths_to_xy(merge_and_sort(cumulative_depths))  
     plot("Cumulative", prices, cumulative_depths)
 
-print_unified(sources)
+#print_unified(sources)
+
+print_individual(sources)
     
