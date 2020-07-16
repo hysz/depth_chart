6505d7454604f031053983ad7e560d839abaf216
Greg Hysen <greg.hysen@gmail.com>
2020-07-15 18:23:22 -0700

    works with raw file

--- plotter.py
+++ plotter.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import collections
+import json
+import sys
 
 # Types
 Depth = collections.namedtuple("Depth", ['input', 'output', 'cum_output']) # input:uint, output:uint, cum_output:uint
@@ -14,15 +16,31 @@ def plot(name, prices, cumulative_depths):
     plt.show()
 
 # Fake response from 0x API
-responseJson = {
+'''
+response_json = {
     "Uniswap": [{"input": 123, "output": 234}],
     "Balancer": [{"input": 124, "output": 458}],
     "0x": [{"input": 123, "output": 456}, {"input": 124, "output": 10}, {"input": 125, "output": 4}]
 }
+'''
+
+
+raw_json = json.load(open('./raw'))
+
+response_json = {}
+for quote_list in raw_json['dexQuotes']:
+    for quote in quote_list:
+        if not quote["source"] in response_json:
+            response_json[quote["source"]] = []
+        response_json[quote["source"]].append({"input": float(quote["input"]), "output": float(quote["output"])})
+
+
+
+
 
 # Parse out sources
 sources = []
-for name,inouts in responseJson.items():
+for name,inouts in response_json.items():
     cum_output = 0
     depths = [Depth(0,0,0)]
     for inout in inouts:
