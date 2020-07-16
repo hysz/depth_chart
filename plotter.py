ab4b4e244299f158f2fac8bf96c1920131ebaedd
Greg Hysen <greg.hysen@gmail.com>
2020-07-15 19:06:55 -0700

    can print cumulative

--- plotter.py
+++ plotter.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import collections
 import json
 import sys
+from functools import cmp_to_key
 
 # Types
 Depth = collections.namedtuple("Depth", ['input', 'output', 'cum_output']) # input:uint, output:uint, cum_output:uint
@@ -14,6 +15,7 @@ def plot(name, prices, cumulative_depths):
     plt.ylabel('Depth')
     plt.xlabel('Price')
     plt.show()
+    #plt.savefig('%s.png'%(name), bbox_inches='tight')
 
 # Fake response from 0x API
 '''
@@ -50,13 +52,43 @@ for name,inouts in response_json.items():
   
 # print(sources)
 
-
-for source in sources:
+def depths_to_xy(depths):
     prices = []
     cumulative_depths = []
-    for depth in source.depths:
+    for depth in depths:
         prices.append(depth.input)
         cumulative_depths.append(depth.cum_output)
-    plot(source.name, prices, cumulative_depths)
-    
+    return (prices, cumulative_depths)
 
+def print_individual(sources):
+    for source in sources:
+        (prices, cumulative_depths) = depths_to_xy(source.depths)
+        plot(source.name, prices, cumulative_depths)
+
+def compare_depths(d1, d2):
+    if d1.input < d2.input:
+        return -1
+    elif d1.input > d2.input:
+        return 1
+    else:
+        return 0
+
+def merge_and_sort(depths):
+    merged_depths = []
+    for depth in sorted(depths,  key=cmp_to_key(compare_depths)):
+        if merged_depths and merged_depths[-1].input == depth.input:
+            print(merged_depths)
+            merged_depths[-1] = Depth(merged_depths[-1].input, -1, merged_depths[-1].cum_output + depth.cum_output)
+        else:
+            merged_depths.append(depth)
+
+    return merged_depths
+
+
+def print_unified(sources):
+    cumulative_depths = [depth for source in sources for depth in source.depths]  
+    (prices, cumulative_depths) = depths_to_xy(merge_and_sort(cumulative_depths))  
+    plot("Cumulative", prices, cumulative_depths)
+
+print_unified(sources)
+    
